9e7b8f5b6b39d366fd55d882f9cb6dd34d9384a3
ADILELBOURKI <adilbcsm@gmail.com>
2020-09-22 10:38:51 +0100

    setting up the project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import bb from "./utils/bb.png";
+import axios from "axios";
+import request from "./utils/request";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      characters: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://www.breakingbadapi.com/api/characters")
+      .then((response) => {
+        const data = response.data;
+        this.setState(() => (this.state.characters = data));
+        console.log(this.state.characters);
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        <img src={bb} />
+        <div className="container">
+          {this.state.characters.map((character) => (
+            <div key={character.char_id}>
+              <img className="img-character" src={character.img} />
+              <h2>{character.name}</h2>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
